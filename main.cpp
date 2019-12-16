ac7d7ed9809fd5b98370657cfabe1e4b6967dcae
Adrian Alvarado <adrianalvarado9090@gmail.com>
2019-12-15 19:19:59 -0600

    agregando método para insertar librerias

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "EDD/ArbolBinario/ABB.h"
 
 //DECLARACIÓN DE METODOS
+string convertir_lower(string data);
 void Menu();
 void Reportes();
 void Reproduccion();
@@ -17,13 +18,18 @@ void PlayLista();
 void CargarLibreria();
 void CargarPlaylist();
 
+//ESTRUCTURAS PARA MANEJAR LA INFORMACIÓN
+ListaDoble *listaArtistas = new ListaDoble();
+
 //USINGS
 using namespace std;
 using json = nlohmann::json;
 
+
 //----------------------------------------------MAIN----------------------------------------------
 int main() {
 
+    Menu();
 
     return 0;
 }
@@ -31,6 +37,14 @@ int main() {
 
 
 //IMPLEMENTACIÓN DE METODOS DECLARADOS
+string convertir_lower(string data){
+    int lower(int c);
+    for_each(data.begin(), data.end(), [](char & c){
+        c = ::tolower(c);
+    });
+    return data;
+}
+
 void Menu(){
     int eleccionM = 0;
 
@@ -192,7 +206,121 @@ void PlayLista(){
     }while(eleccionPL != 0);
 }
 
+void CargarPlaylist(){
 
+}
+
+bool validarArtista(json artista){
+    if(!artista.is_null() && !artista["Name"].is_null() && !artista["Albums"].is_null()){
+        return true;
+    }
+    return false;
+}
+
+bool validarAlbum(json album){
+    if(!album["Name"].is_null() && !album["Month"].is_null() && !album["Year"].is_null() && !album["Songs"].is_null()){
+        return true;
+    }
+    return false;
+}
+
+void agregarAlbums(string album, string mes, string anio, Cuboortogonal* cuboAlbums){
+    if(mes == "enero"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->enero, atoi(anio.c_str()) );
+    }
+
+    if(mes == "febrero"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->febrero, atoi(anio.c_str()) );
+    }
+
+    if(mes == "marzo"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->marzo, atoi(anio.c_str()) );
+    }
+
+    if(mes == "abril"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->abril, atoi(anio.c_str()) );
+    }
+
+    if(mes == "mayo"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->mayo, atoi(anio.c_str()) );
+    }
+
+    if(mes == "junio"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->junio, atoi(anio.c_str()) );
+    }
+
+    if(mes == "julio"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->julio, atoi(anio.c_str()) );
+    }
+
+    if(mes == "agosto"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->agosto, atoi(anio.c_str()) );
+    }
+
+    if(mes == "septiembre"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->septiembre, atoi(anio.c_str()) );
+    }
+
+    if(mes == "octubre"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->octubre, atoi(anio.c_str()) );
+    }
+
+    if(mes == "noviembre"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->noviembre, atoi(anio.c_str()) );
+    }
+
+    if(mes == "diciembre"){
+        cuboAlbums->agregarcubo(album, cuboAlbums->diciembre, atoi(anio.c_str()) );
+    }
+}
+
+void CargarLibreria(){
+    string filename = "";
+
+    cout << "Nombre de la libreria: ";
+    cin >> filename;
+
+    cout << "Cargando archivo " << filename << endl << endl;
+    ifstream reader(filename);
+
+    if (reader.fail()){
+        cout << "El archivo no existe, verifique que la ruta y el archivo exista." << endl << endl;
+    }else{
+        cout << "Cargando " << filename << endl << endl;
+
+        json libreria;
+        reader >> libreria;
+        libreria = libreria["Library"];
+
+        for(int i = 0; i < libreria.size(); i++){
+            json artista = libreria[i]["Artist"];
+
+            if(validarArtista(artista)){
+                string nombreArtista = artista["Name"];
+                json albumsArtista = artista["Albums"];
+                Cuboortogonal *cuboAlbums = new Cuboortogonal();
+
+                for(int j = 0; j < albumsArtista.size(); j++){
+                    json album = albumsArtista[j];
+
+                    if(validarAlbum(album)){
+                        string nombreAlbum = album["Name"];
+                        string mesAlbum = convertir_lower(album["Month"]);
+                        string anioAlbum = album["Year"];
+                        json cancionesAlbum = album["Songs"];
+
+                        agregarAlbums(nombreAlbum, mesAlbum, anioAlbum, cuboAlbums);
+
+                    }
+                }
+                listaArtistas->replaceChars(nombreArtista, "\"", "");
+                listaArtistas->insertarartista(nombreArtista, cuboAlbums);
+            }
+        }
+        listaArtistas->imprimiralbums();
+    }
+    reader.close();
+}
 
 
 
